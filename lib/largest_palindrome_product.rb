269a392e17c433f0236d40a39d99b97638ea57e8
EJLarcs <ejlarcombe@gmail.com>
2015-10-12 23:26:31 -0400

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+# Your objective is to find the largest palindrome made from the product of two three-digit numbers.
+ def largest_palindrome_product
+   result = nil
+   (800..999).each { |n|
+     (800..999).each { |i|
+       sum = n * i
+       if is_palindrome?(sum)
+         result = sum
+       end
+     }
+   }
+   result
+ end
+
+ def is_palindrome?(result)
+   return true if result.to_s == result.to_s.reverse
+ end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def answer
+    result = nil
+    (800..999).each { |n|
+      (800..999).each { |i|
+        sum = n * i
+        if is_palindrome?(sum)
+          result = sum
+        end
+      }
+    }
+    result
+  end
+
+  def is_palindrome?(result)
+    return true if result.to_s == result.to_s.reverse
+  end
+
+end
